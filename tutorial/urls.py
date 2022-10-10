54b87d827a7bb285df6db24ed4b07aac32ce16f5
Macduff Olusa <42511643+tobiolusa@users.noreply.github.com>
2022-10-10 14:25:10 +0100

    router.urls corrected

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -23,6 +23,6 @@ router.register(r'users', views.UserViewSet)
 router.register(r'groups', views.GroupViewSet)
 
 urlpatterns = [
-    path('', include('router.urls')),
+    path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
